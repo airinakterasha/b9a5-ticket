e745d2891840abb59f68f41f1ab48dc337e5937d
Airin Akter <airinmoni.bd@gmail.com>
2024-02-19 23:13:11 +0600

    almost ready

---
+++ js/nextfun.js
@@ -0,0 +1,8 @@
+function nextClick(){
+    //console.log('hujfb');
+    const successCont = document.getElementById('nextSuccess');
+    console.log(successCont);
+    successCont.classList.remove('hidden');
+    successCont.classList.add('visible');
+}
+
